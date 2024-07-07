35cdd02fe4948973b62df1657bb9d8664905eb44
NeoG14 <nicolaplatajob@gmail.com>
2024-07-07 10:56:07 -0300

    testing divide algorithm and insertion in general

--- tree-web-app/src/test/java/com/neog14/treewebapp/BTreeNodeTest.java
+++ tree-web-app/src/test/java/com/neog14/treewebapp/BTreeNodeTest.java
@@ -46,7 +46,62 @@ public class BTreeNodeTest {
 //    }
 
     @Test
-    public void testDivideArray(){
+    public void testDivideArrayEvenOrder(){
+        nodeEvenOrder.insert(1);
+        nodeEvenOrder.insert(4);
+        nodeEvenOrder.insert(9);
+
+        BTreeNode actualNode = nodeEvenOrder;
+        BTreeNode newNode = new BTreeNode(nodeEvenOrder.getOrder(),true,nodeEvenOrder.getRoot());
+
+        int[] splittedArray = Arrays.copyOf(actualNode.getKeys(),nodeEvenOrder.getOrder());//creo el arreglo con +1 de espacio
+        // estado actual del arreglo de claves (lleno) 1,4,9
+        nodeEvenOrder.insertAndKeepSorted(splittedArray,5); //Supongo que inserto el numero 5
+
+        int promoted = nodeEvenOrder.divideArray(splittedArray,actualNode,newNode);
+        assertEquals(5,promoted);
+
+        assertEquals(1,actualNode.getKeys()[0]);
+        assertEquals(4,actualNode.getKeys()[1]);
+
+        assertEquals(9,newNode.getKeys()[0]);
+    }
+
+    @Test //En caso de ser de orden impar el nodo de la izquierda queda siemprecon un elemento mas que el nuevo nodo
+    public void testDivideArrayOddOrder(){
+        nodeOddOrder.insert(1);
+        nodeOddOrder.insert(2);
+        nodeOddOrder.insert(7);
+        nodeOddOrder.insert(9);
+
+        BTreeNode actualNode = nodeOddOrder;
+        BTreeNode newNode = new BTreeNode(nodeOddOrder.getOrder(),true,nodeOddOrder.getRoot());
+
+        int[] splittedArray = Arrays.copyOf(actualNode.getKeys(),nodeOddOrder.getOrder());//creo el arreglo con +1 de espacio
+        // estado actual del arreglo de claves (lleno) 1,2,7,9
+        nodeOddOrder.insertAndKeepSorted(splittedArray,5); //Supongo que inserto el numero 5
+
+
+        int promoted = nodeOddOrder.divideArray(splittedArray,actualNode,newNode);
+        assertEquals(5,promoted);
+
+        assertEquals(1,actualNode.getKeys()[0]);
+        assertEquals(2,actualNode.getKeys()[1]);
+
+        assertEquals(7,newNode.getKeys()[0]);
+        assertEquals(9,newNode.getKeys()[1]);
+    }
+
+    @Test
+    public void testInsertOnFullRoot(){
+        nodeOddOrder.insert(1);
+        nodeOddOrder.insert(2);
+        nodeOddOrder.insert(7);
+        nodeOddOrder.insert(9);
+
+        nodeOddOrder.insert(5);
+
+
 
     }
 }
