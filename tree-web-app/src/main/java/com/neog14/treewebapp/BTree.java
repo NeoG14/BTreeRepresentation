dd68482bfa910f197563d580c14d4697431c64b2
NeoG14 <nicolaplatajob@gmail.com>
2024-07-13 23:34:53 -0300

    working on logic

--- tree-web-app/src/main/java/com/neog14/treewebapp/BTree.java
+++ tree-web-app/src/main/java/com/neog14/treewebapp/BTree.java
@@ -1,5 +1,8 @@
 package com.neog14.treewebapp;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class BTree {
     int order;
     BTreeNode root;
@@ -23,47 +26,95 @@ public class BTree {
         }
     }
 
+
+
     public void insert(int key) {
-        if (root.leaf){ //primer nodo
-            if (root.num_keys<max_keys) {//si tengo espacio en al raiz
-                root.insert_non_full(key);
-            } else {//raiz llena
-                    BTreeNode new_node = new BTreeNode(true,order,num_nodes);
-                    num_nodes++;
-                    BTreeNode new_root = new BTreeNode(false,order,num_nodes);
-                    num_nodes++;
-                    root.split_child(new_node,key);
-
-                    new_root.children[0] = root;
-                    new_root.children[1] = new_node;
-                    root=new_root;
-
-                }
+        BTreeNode node_to_insert = root.search_to_insert(key);
+        if(node_to_insert != null && !node_to_insert.full(max_keys)){
+            node_to_insert.insert_non_full(key);
+        }else {
+            if(node_to_insert != null && node_to_insert.full(max_keys)){
+                BTreeNode new_node = new BTreeNode(true;order,num_nodes);
+                num_nodes++;
+                int promoted = split_node(node_to_insert,new_node,key);
             }
         }
 
-
-
-//        if (root == null) {
-//            root = new BTreeNode(true, order,0);
-//            root.keys[0] = key;
-//            root.num_keys = 1;
-//        } else {
-//            if (root.num_keys == root.max_keys) {
-//                BTreeNode s = new BTreeNode(false, order,root.num_node+1);
-//                s.children[0] = root;
-//                s.split_child(0, root);
-//                int i = 0;
-//                if (s.keys[0] < key) {
-//                    i++;
-//                }
-//                s.children[i].insert_non_full(key);
-//                root = s;
-//            } else {
-//                root.insert_non_full(key);
-//            }
-//        }
     }
 
+    public int split_node(BTreeNode left_node, BTreeNode right_node, int key){
+        Integer[] split = Arrays.copyOf(left_node.getKeys(),order);
+        Integer[] left_array = new Integer[max_keys];
+        Integer[] right_array = new Integer[max_keys];
+        int promoted;
+        split[order-1]=key;
+        split = Arrays.stream(split)
+                .sorted().
+                toArray(Integer[]::new);
+        int i=0;
+        if((split.length % 2) == 0){
 
+        }else {
+            while(i<split.length/2){
+                left_array[i]=split[i];
+                i++;
+            }
+            promoted = split[i];
+            i++;
+            while (i<split.length){
+                right_array[i]=split[i];
+                i++;
+            }
+
+        }
+        return 4;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public BTreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(BTreeNode root) {
+        this.root = root;
+    }
+
+    public int getT() {
+        return t;
+    }
+
+    public void setT(int t) {
+        this.t = t;
+    }
+
+    public int getMax_keys() {
+        return max_keys;
+    }
+
+    public void setMax_keys(int max_keys) {
+        this.max_keys = max_keys;
+    }
+
+    public int getMin_keys() {
+        return min_keys;
+    }
+
+    public void setMin_keys(int min_keys) {
+        this.min_keys = min_keys;
+    }
+
+    public int getNum_nodes() {
+        return num_nodes;
+    }
+
+    public void setNum_nodes(int num_nodes) {
+        this.num_nodes = num_nodes;
+    }
 }
--- tree-web-app/src/main/java/com/neog14/treewebapp/BTreeNode.java
+++ tree-web-app/src/main/java/com/neog14/treewebapp/BTreeNode.java
@@ -3,11 +3,11 @@ package com.neog14.treewebapp;
 import java.util.Arrays;
 
 public class BTreeNode {
-    boolean leaf;
-    Integer[] keys;
-    BTreeNode[] children;
-    int num_node;
-    int num_keys;
+    private boolean leaf;
+    private Integer[] keys;
+    private BTreeNode[] children;
+    private int num_node;
+    private int num_keys;
 
     public BTreeNode(boolean leaf, int order, int num_node){
         this.leaf = leaf;
@@ -15,7 +15,27 @@ public class BTreeNode {
         this.children = new BTreeNode[order];
         this.num_keys = 0;
         this.num_node = num_node;
+    }
 
+    public BTreeNode search_to_insert(Integer key){
+        boolean contains = Arrays.stream(keys).anyMatch(key::equals);
+        if (leaf && !contains){
+            return this;
+        } else{
+            if(contains)
+                return null;
+            else {
+                int i = 0;
+                while (keys[i]!=null && key > keys[i]){
+                    i++;
+                }
+                return children[i].search_to_insert(key);
+            }
+        }
+    }
+
+    public boolean full(int max_keys){
+        return (max_keys==num_keys);
     }
 
     @Override
@@ -38,21 +58,17 @@ public class BTreeNode {
     }
 
     public void insert_non_full(Integer key){
-        int i=0;
-        boolean contains = Arrays.stream(keys).anyMatch(key::equals);
-        if (!contains){
-            while(keys[i] != null && key>keys[i]){
-                i++;
-            }
-            for (int j=num_keys; i>j; j--)
-                keys[j] = keys[j-1];
-            keys[i]=key;
-            num_keys++;
-            System.out.println("Key " + key +"inserted in node: "+num_node);
-        }else {
-            System.out.println("Key "+ key +"already exist in tree");
+        int i = 0;
+        while(keys[i] != null && key>keys[i]){
+            i++;
         }
+        for (int j=num_keys; i>j; j--)
+            keys[j] = keys[j-1];
+        keys[i]=key;
+        num_keys++;
+        System.out.println("Key " + key +"inserted in node: "+num_node);
     }
+
 
     public void split_child(BTreeNode left_child, int key){
         Integer[] aux = Arrays.copyOf(keys,num_keys+1);//copio de keys en aux con +1 de DimF
@@ -83,8 +99,6 @@ public class BTreeNode {
 
 
 
-
-
         int i;
         for (i = 0; i < num_keys; i++) {
             if (!leaf) {
@@ -101,200 +115,43 @@ public class BTreeNode {
     }
 
 
+    public boolean isLeaf() {
+        return leaf;
+    }
 
-//
-//    //si no se pudo insertar (false) es que hay que crear otro nodo
-//    private boolean insertKey(BTreeNode node, int key){
-//        if ((this.isRoot || this.isLeaf) && nunKeys<maxKeys) {
-//            int i= 0;
-//            while(node.getKeys()[i]!=0 && node.getKeys()[i]<key){
-//                i++;
-//            }
-//            this.insertAndKeepSorted(node.getPointers()[i].getKeys(),key);
-//            this.nunKeys++;
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//    public void insertAndKeepSorted(int[] array, int key){
-//        if(nunKeys>0){
-//            int i = nunKeys-1;
-//            while(i>=0 && array[i]>key && array[i] != key){
-//                array[i+1] = array[i];//realizo un corrimiento de los elementos
-//                i--;
-//            }
-//            array[i+1]=key;
-//        }else{
-//            array[0] = key; //Inserto el primer espacio
-//        }
-//    }
-//
-//    //este metodo crea los arreglos para la redistribucion y los asigna a los nodos correspondientes retorna el elemento a promocionar
-//    //En caso de ser de orden impar el nodo de la izquierda queda siempre con un elemento mas que el nuevo nodo
-//    private int divideArray(int[] splitArray, BTreeNode firstNode, BTreeNode secondNode){
-//        int[] firstArray = new int[maxKeys];
-//        int[] secondArray = new int[maxKeys];
-//
-//        int firstHalf = splitArray.length/2;
-//        int secondHalf = splitArray.length;
-//
-//        //int [] array = firstNode.getKeys();
-//        int value=0;
-//
-//        for (int i=0; i<firstHalf ;i++){
-//            firstArray[i] = splitArray[i];
-//        }
-//
-//        int promoted = splitArray[firstHalf];//para evitar tener que hacer un corrimiento nuevamente al realizar la promocion en arbol b+
-//
-//        for (int i=firstHalf+1; i<secondHalf ;i++){
-//            secondArray[value] = splitArray[i];
-//            value++;
-//        }
-//
-//        firstNode.setKeys(firstArray);// seteo el arreglo distribuido
-//        firstNode.setNunKeys(firstHalf); //seteo el numero de claves que quedan en el primer nodo
-//
-//        //System.out.println(firstNode);
-//
-//        secondNode.setKeys(secondArray);// seteo el arreglo distribuido
-//        secondNode.setNunKeys((secondHalf-firstHalf)-1);//seteo el numero de claves que quedan en el segundo nodo
-//
-//        //System.out.println(secondNode);
-//        return promoted;
-//    }
-//
-//
-//    public void printTree() {
-//        String treeString = "";
-//        treeString += "RootNode: "+ root.getNumNode()+":";
-//        for (int i=0; i<this.maxKeys; i++){
-//
-//            if(root.getPointers()[i] != null)
-//                treeString += root.getPointers()[i].getNumNode();
-//
-//            treeString += "(" + root.getKeys()[i] + ")";
-//        }
-//        System.out.println(treeString);
-//        int i=0;
-//        while(root.getPointers()[i] != null){
-//            System.out.print(printNode(root.getPointers()[i]));
-//            System.out.print("  ");
-//            i++;
-//        }
-//    }
-//
-//    private String printNode(BTreeNode node){
-//        String nodeString = "Node: " + node.numNode +": ";
-//        if (!isLeaf){
-//            int i=0;
-//            while(this.getPointers()[i] != null){
-//                printNode(this.getPointers()[i]);
-//                i++;
-//            }
-//        }else {
-//            for (int i=0; i<node.maxKeys; i++){
-//                nodeString += "(" + node.getKeys()[i] + ")";
-//            }
-//        }
-//        return nodeString;
-//    }
-//
-//    // no se si funciona
-//    private void addPointer(BTreeNode actualNode, BTreeNode newNode) {
-//        this.pointers[this.nunKeys-1] = actualNode;
-//        this.pointers[this.nunKeys] = newNode;
-//        int s = 5;
-//    }
-//
-//
-//    public int getOrder() {
-//        return order;
-//    }
-//
-//    public void setOrder(int order) {
-//        this.order = order;
-//    }
-//
-//    public BTreeNode[] getPointers() {
-//        return pointers;
-//    }
-//
-//    public void setPointers(BTreeNode[] pointers) {
-//        this.pointers = pointers;
-//    }
-//
-//    public int[] getKeys() {
-//        return keys;
-//    }
-//
-//    public void setKeys(int[] keys) {
-//        this.keys = keys;
-//    }
-//
-//    public boolean isLeaf() {
-//        return isLeaf;
-//    }
-//
-//    public void setLeaf(boolean leaf) {
-//        this.isLeaf = leaf;
-//    }
-//
-//    public boolean isRoot() {
-//        return isRoot;
-//    }
-//
-//    public void setIsRoot(boolean root) {
-//        this.isRoot = root;
-//    }
-//
-//    public int getNunKeys() {
-//        return nunKeys;
-//    }
-//
-//    public void setNunKeys(int nunKeys) {
-//        this.nunKeys = nunKeys;
-//    }
-//
-//    public int getMinKeys() {
-//        return minKeys;
-//    }
-//
-//    public void setMinKeys(int minKeys) {
-//        this.minKeys = minKeys;
-//    }
-//
-//    public int getMaxKeys() {
-//        return maxKeys;
-//    }
-//
-//    public void setMaxKeys(int maxKeys) {
-//        this.maxKeys = maxKeys;
-//    }
-//
-//    public static int getTotalNodes() {
-//        return totalNodes;
-//    }
-//
-//    public static void setTotalNodes(int totalNodes) {
-//        BTreeNode.totalNodes = totalNodes;
-//    }
-//
-//    public int getNumNode() {
-//        return numNode;
-//    }
-//
-//    public void setNumNode(int numNode) {
-//        this.numNode = numNode;
-//    }
-//
-//    public BTreeNode getRoot() {
-//        return root;
-//    }
-//
-//    public void setRoot(BTreeNode root) {
-//        this.root = root;
-//    }
+    public void setLeaf(boolean leaf) {
+        this.leaf = leaf;
+    }
 
+    public Integer[] getKeys() {
+        return keys;
+    }
+
+    public void setKeys(Integer[] keys) {
+        this.keys = keys;
+    }
+
+    public BTreeNode[] getChildren() {
+        return children;
+    }
+
+    public void setChildren(BTreeNode[] children) {
+        this.children = children;
+    }
+
+    public int getNum_node() {
+        return num_node;
+    }
+
+    public void setNum_node(int num_node) {
+        this.num_node = num_node;
+    }
+
+    public int getNum_keys() {
+        return num_keys;
+    }
+
+    public void setNum_keys(int num_keys) {
+        this.num_keys = num_keys;
+    }
 }
